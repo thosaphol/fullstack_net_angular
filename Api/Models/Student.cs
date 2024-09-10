27a8d974e2676ba5343132f35a44c40f65dcba0c
thosaphol <thosaphol@outlook.co.th>
2024-09-10 15:37:28 +0700

    [MOD] Change property Emal to Email

--- Api/Models/Student.cs
+++ Api/Models/Student.cs
@@ -11,5 +11,5 @@ public int Id {get;set;}
 public string Name {get;set;}
 public string? Address {get;set;}
 public string? PhoneNumber {get;set;}
-public string? Emal {get;set;}
+public string? Email {get;set;}
 }
