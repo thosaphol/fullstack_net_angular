650ab95653af991cb34fdffd0b0f619033efcaf9
thosaphol <thosaphol@outlook.co.th>
2024-09-10 23:41:40 +0700

    [ADD] Add students service

---
+++ client/src/app/services/students.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentsService {
+
+  constructor(private http:HttpClient) { }
+}
