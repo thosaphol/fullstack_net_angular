882064b0f53215163e0a594c027266cc616081f9
thosaphol <thosaphol@outlook.co.th>
2024-09-10 14:43:07 +0700

    [MOD] new Get Students action

--- Api/Controllers/StudentsController.cs
+++ Api/Controllers/StudentsController.cs
@@ -1,6 +1,8 @@
 using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -10,13 +12,16 @@ namespace Api.Controllers
     {
         private readonly AppDbContext _dbContext;
 
-        public StudentsController(AppDbContext dbContext){
+        public StudentsController(AppDbContext dbContext)
+        {
             _dbContext = dbContext;
         }
-        
+
         [HttpGet]
-        public string SayHi(){
-            return "Hi";
+        public async Task<IEnumerable<Student>> Students()
+        {
+            var students = await _dbContext.Students.AsNoTracking().ToListAsync();
+            return students;
         }
     }
 }
