9a45fa0ec35cdc406d74727764860f0490c78c05
thosaphol <thosaphol@outlook.co.th>
2024-09-10 23:29:10 +0700

    [MOD] Add Students route and routerlink to app component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'students',
+        component:StudentsComponent
+    }
+];
