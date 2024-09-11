1f1a72bd554ed79a1e3dc5d59c7ae7df0402cf9a
thosaphol-ttsb <thosaphol_v@tripetch-it.co.th>
2024-09-11 16:32:28 +0700

    [MOD] Create getStudents method

--- client/src/app/services/students.service.ts
+++ client/src/app/services/students.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../types/student';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,6 @@ import { Injectable } from '@angular/core';
 export class StudentsService {
 
   constructor(private http:HttpClient) { }
+
+  getStudents = ():Observable<Student[]> => this.http.get<Student[]>("http://localhost:5148/api/Students")
 }
