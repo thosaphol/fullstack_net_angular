6fe63784fade1e678910b84f1f108d7e93198f04
thosaphol-ttsb <thosaphol_v@tripetch-it.co.th>
2024-09-11 20:13:29 +0700

    [MOD] Get All student on init  component

--- client/src/app/students/students.component.ts
+++ client/src/app/students/students.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { StudentsService } from '../services/students.service';
+import { Observable } from 'rxjs';
+import { Student } from '../types/student';
+import { AsyncPipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe,TitleCasePipe],
   templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit{
+  
+  students$!:Observable<Student[]>
+  studentsService = inject(StudentsService)
 
+  ngOnInit(): void {
+    this.students$ = this.studentsService.getStudents()
+    
+  }
 }
